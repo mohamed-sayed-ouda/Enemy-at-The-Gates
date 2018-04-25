84c655bd80b982232e72d9377bb190a9616f9bd5
RamyFCIH <38047800+RamyFCIH@users.noreply.github.com>
2018-04-25 21:52:24 +0200

    Enviroment
    Pushing the whole enviroment including UI

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Vector3 center;
+    public Vector3 v = Vector3.zero;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        v = v + 0.0001f * (center - transform.position).normalized;
+        transform.position = transform.position + v;
+        transform.LookAt(transform.position + v);
+        MoveCenter();
+
+    }
+
+    void MoveCenter()
+    {
+        Vector2 rand = 5 * Random.insideUnitCircle;
+        Vector3 v3 = new Vector3(rand.x, 0, rand.y);
+        center = center + v3;
+
+
+    }
+}
