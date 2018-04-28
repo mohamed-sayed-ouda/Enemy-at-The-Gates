5125f5568bb7cee7df05801ad655976e7e612010
mohamed-sayed-ouda <38180592+mohamed-sayed-ouda@users.noreply.github.com>
2018-04-28 17:38:54 +0200

    Prefabs,Scenes,Scripts
    1)Ramy ayman ahmed Task:Game Enviroment,Game UI,Camera Movement
    2)Khaled Essam: Hero Player Implementation
    3)Mohamed Sayed:Ranger Player  Implementation
    4)Mohamed Sayed Ouda:Enemy Implementation
    5)Hesham Mohamed :Enemy Implementation

---
+++ Scripts/GameManager.cs
@@ -0,0 +1,195 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using UnityEngine.SceneManagement;
+using UnityEngine.Experimental.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject[] spawnPoints;
+    [SerializeField] GameObject[] powerUpSpawns;
+    [SerializeField] GameObject tanker;  
+    [SerializeField] GameObject soldier;
+    [SerializeField] GameObject arrow;
+    [SerializeField] GameObject healthPowerUp;
+    [SerializeField] Text levelText;
+    [SerializeField] Text endGameText;
+    [SerializeField] int maxPowerUps = 4;
+    [SerializeField] int finalLevel = 2;
+   // [SerializeField] GameObject PauseMenu;
+    private bool isPauseMenu = false;
+    private bool gameOver = false;
+    private int currentLevel;
+    private float generatedSpawnTime = 1;
+    private float currentSpawnTime = 0;
+    private float powerUpSpawnTime = 30;
+    private float currentPowerUpSpawnTime = 0;
+    private GameObject newEnemy;
+    private int powerups = 0;
+    private GameObject newPowerup;
+    
+    private List<EnemyHealth> enemies = new List<EnemyHealth>();
+    private List<EnemyHealth> killedEnemies = new List<EnemyHealth>();
+
+    public void RegisterEnemy(EnemyHealth enemy)
+    {
+        enemies.Add(enemy);
+    }
+
+    public void KilledEnemy(EnemyHealth enemy)
+    {
+        killedEnemies.Add(enemy);
+    }
+
+    public void RegisterPowerUp()
+    {
+        powerups++;
+    }
+
+    public bool GameOver
+    {
+        get { return gameOver; }
+    }
+
+    public GameObject Player
+    {
+        get { return player; }
+    }
+
+    public GameObject Arrow
+    {
+        get { return arrow; }
+    }
+
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        currentLevel = 1;
+        
+        endGameText.GetComponent<Text>().enabled = false;
+       // PauseMenu.SetActive(false);
+        StartCoroutine(spawn());
+        StartCoroutine(powerUpSpawn());
+    } 
+
+    void Update()
+    {
+
+        currentSpawnTime += Time.deltaTime;
+        currentPowerUpSpawnTime += Time.deltaTime;
+            
+        
+
+    }
+    
+    public void PlayerHit(int currentHP)
+    {
+
+        if (currentHP > 0)
+        {
+            gameOver = false;
+        }
+        else
+        {
+            gameOver = true;
+            StartCoroutine(endGame("Defeat"));
+        }
+    }
+
+    IEnumerator spawn()
+    {
+
+        if (currentSpawnTime > generatedSpawnTime)
+        {
+            currentSpawnTime = 0;
+
+            if (enemies.Count < currentLevel)
+            {
+
+                int randomNumber = Random.Range(0, spawnPoints.Length - 1);
+                GameObject spawnLocation = spawnPoints[randomNumber];
+                int randomEnemy = Random.Range(0, 2);
+                if (randomEnemy == 0)
+                {
+                    newEnemy = Instantiate(soldier) as GameObject;
+                }
+               
+                else if (randomEnemy == 1)
+                {
+                    newEnemy = Instantiate(tanker) as GameObject;
+                }
+
+                newEnemy.transform.position = spawnLocation.transform.position;
+
+            }
+
+            if (killedEnemies.Count == currentLevel && currentLevel != finalLevel)
+            {
+
+                enemies.Clear();
+                killedEnemies.Clear();
+
+                yield return new WaitForSeconds(3f);
+                currentLevel++;
+                levelText.text = "Level " + currentLevel;
+            }
+
+            if (killedEnemies.Count == finalLevel)
+            {
+                StartCoroutine(endGame("Victory!"));
+            }
+        }
+
+        yield return null;
+        StartCoroutine(spawn());
+    }
+
+    IEnumerator powerUpSpawn()
+    {
+
+        if (currentPowerUpSpawnTime > powerUpSpawnTime)
+        {
+            currentPowerUpSpawnTime = 0;
+
+            if (powerups < maxPowerUps)
+            {
+
+                int randomNumber = Random.Range(0, powerUpSpawns.Length - 1);
+                GameObject spawnLocation = powerUpSpawns[randomNumber];
+                    newPowerup = Instantiate(healthPowerUp) as GameObject;
+            
+                newPowerup.transform.position = spawnLocation.transform.position;
+            }
+        }
+
+        yield return null;
+        StartCoroutine(powerUpSpawn());
+    }
+
+    IEnumerator endGame(string outcome)
+    {
+
+        endGameText.text = outcome;
+        endGameText.GetComponent<Text>().enabled = true;
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("sample_scene");
+    }
+}
---
+++ Scripts/Menu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+    [SerializeField]
+    private Text Pause;
+
+   
+
+    void Awake() {
+
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    
+
+    public void Ranger() {
+		SceneManager.LoadScene ("Ranger");
+	}
+    public void Girl()
+    {
+        SceneManager.LoadScene("BarbrianGirl");
+    }
+
+	public void Quit() {
+
+        UnityEditor.EditorApplication.isPlaying = false;
+	}
+    public void Start_Pause()
+    {
+
+        if (Time.timeScale == 1)
+        {
+            Time.timeScale = 0;
+            Pause.text = "Start";
+        }
+        else
+        {
+            Time.timeScale = 1;
+            Pause.text = "Pause";
+
+        }
+
+    }
+}
